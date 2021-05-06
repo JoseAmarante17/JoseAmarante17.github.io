11f967b6a699c76ef3bde4344699d25735e1ade3
JoseAmarante17 <83371516+JoseAmarante17@users.noreply.github.com>
2021-05-06 04:41:27 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,68 +1,41 @@
-//when the user submits
-function pay() {
-   
-    if(document.getElementById("output-total").value == 0.00){
-        alert("Enter an Amount");
+//GLOBAL VARIABLES
+function sound(soundObj) {
+    let sound = document.getElementById(soundObj);
+    alert("Take a 10 minuite break.");
+    sound.onplay;
+}
+
+function check() {
+    if (document.getElementById("input").value == 0) {
+        alert("ENTER A NUMBER");
     }
-    else {
-         alert("You payment has been processed. Thank you for dinning with us. Please come again.");
+}
+
+function algo() {
+    if (document.getElementById("input").value == 0) {
+        alert("ENTER A NUMBER");
+    } else {
+        //user input
+        let mins = document.getElementById("input").value;
+        let realMins = mins * 60;
+        //SETS THE MAX VALUE OF THE PROGRESS BAR
+        document.getElementById("progressBar").max = realMins
+        console.log("Working");
+
+        //the math behind the progress bar
+        let pomodoro = setInterval(function () {
+            if (realMins <= 0) {
+                clearInterval(pomodoro);
+                sound("sound1");
+            }
+            document.getElementById("progressBar").value = realMins - 1;
+            realMins -= 1;
+        }, 1000);
+
+        console.log(realMins);
     }
 }
 
-//Shows the percentage and 
-function calculate() {
 
-    //variables input
-    let tipRange = document.getElementById("tip-range").value;
-    let amount = Number(document.getElementById("amount").value);
-    //prints the percent value to screen
-    percent(tipRange);
-    
-    //VARIABLES OUTPUT 
-    let totalTip = document.getElementById("output-justTip");
-    let totalAmountTip = document.getElementById("output-total");
 
-    //calculates the totals and dispalys
-    let tip = (tipRange / 100)
-    totalTip.value = Number(amount * tip).toFixed(2);
-    totalAmountTip.value= Number(amount + Number(totalTip.value)).toFixed(2);
 
-    //displays Block
-    display();
-}
-
-function calcper(){
-
-    //variables input
-    let tipRange = document.getElementById("range-print").value;
-    let amount = Number(document.getElementById("amount").value);
-
-    //VARIABLES OUTPUT 
-    let totalTip = document.getElementById("output-justTip");
-    let totalAmountTip = document.getElementById("output-total");
-
-    //calculates the totals and dispalys
-    let tip = (tipRange / 100)
-    totalTip.value = Number(amount * tip).toFixed(2);
-    totalAmountTip.value = Number(amount + Number(totalTip.value)).toFixed(2);
-
-    //SETS VALUE OF THE RANGE TO THE TEXT BOX
-    let txt = (document.getElementById("range-print").value);
-    document.getElementById("tip-range").value = (txt) ;
-    console.log (txt.value = (document.getElementById("tip-range").value))
-    display();
-}
-
-//Displays percentages
-function percent(x) {
-    let p = document.getElementById("range-print");
-    p.value = (x )
-
-}
-
-//displays values 
-function display() {
-    //show the results when the tip is interacted with
-    let results = document.getElementById("output-area");
-    results.style.display = "block";
-}
